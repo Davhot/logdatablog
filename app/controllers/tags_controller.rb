0224d049a3c61928c456625fb1d996b60a3b0b9e
David <david.home@mail.ru>
2018-08-22 10:27:06 +0300

    CRUD Tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,7 @@
 class TagsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_breadcrumbs
+  before_action :set_tag, only: %i(show edit update destroy)
 
   def index
     @items = Tag.all
@@ -13,14 +14,37 @@ class TagsController < ApplicationController
   def create
     @item = Tag.new(tag_params)
     if @item.save
-      redirect_to tags_path
+      redirect_to tags_path, flash: {success: 'Тег успешно создан'}
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update_attributes(tag_params)
+      redirect_to tag_path(@item), flash: {success: 'Тег успешно обновлен'}
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to tags_path, flash: {success: 'Тег успешно удален'}
+    else
+      redirect_to tags_path, flash: {error: 'Тег не удален'}
+    end
+  end
+
   private
-  
+
+  def set_tag
+    @item = Tag.find(params[:id])
+  end
+
   def set_breadcrumbs
     @need_restful_breadcrumbs = true
   end
