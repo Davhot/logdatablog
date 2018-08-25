767f9471de22c47696da13559c628177fa8d76e0
David <david.home@mail.ru>
2018-08-25 20:37:29 +0300

    crud article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_breadcrumbs, except: [:index, :show]
+  before_action :set_breadcrumbs, except: [:index]
+  before_action :set_article, only: %i(show edit update destroy)
 
   def index
     @items = Article.all
@@ -26,14 +27,24 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if @item.update_attributes(article_params)
+      redirect_to article_path(@item), flash: {success: 'Статья успешно обновлена'}
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @item.destroy
+      redirect_to articles_path, flash: {success: 'Статья успешно удалена'}
+    else
+      redirect_to articles_path, flash: {error: 'Статья не удалена'}
+    end
   end
 
   private
 
-  def set_acrticle
+  def set_article
     @item = Article.find(params[:id])
   end
 
