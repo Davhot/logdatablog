56f8ac76ca570a6b922c4eb0b1b140d659cded17
David <david.home@mail.ru>
2018-08-26 15:59:26 +0300

    fix assets summernote

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '192.168.111.111', user: 'david', roles: %w{app db}
 
-set :branch, ENV['branch'] || 'article'
+set :branch, ENV['branch'] || 'develop'
 
 set :default_env, {
   'RAILS_ENV': 'production'
