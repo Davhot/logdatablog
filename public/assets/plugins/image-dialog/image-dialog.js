8e26ac65dbd00648fffd765724585abc841e671b
David <david.home@mail.ru>
2018-10-08 22:44:05 +0300

    configure Nginx

--- public/assets/plugins/image-dialog/image-dialog.js
+++ public/assets/plugins/image-dialog/image-dialog.js
@@ -1,14 +1,3 @@
-/*!
- * Image (upload) dialog plugin for Editor.md
- *
- * @file        image-dialog.js
- * @author      pandao
- * @version     1.3.4
- * @updateTime  2015-06-09
- * {@link       https://github.com/pandao/editor.md}
- * @license     MIT
- */
-
 (function() {
 
     var factory = function (exports) {
