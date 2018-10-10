f65571a352dc731eed7cec1b8b788029957bfe02
Хотелов Давид Александрович <dkhotelov@phoenixit.ru>
2018-10-10 17:52:11 +0300

    add permission to download files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :download_file]
   before_action :set_breadcrumbs, except: [:index]
   before_action :set_article, only: %i(show edit update destroy)
   before_action :set_article_files, only: %i(edit update)
