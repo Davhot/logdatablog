31ccf00099ffd902c367104e77bd1dded3610aa0
David <david.home@mail.ru>
2018-10-25 22:11:17 +0300

    control comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!, only: [:comments]
+
   def about
   end
 
   def contact
+  end
+
+  def comments
+    @comments = Article.all.map{|x| [x.title, x.comments]}.select{|x| x[1].size > 0}
   end
 end
