edb60584bf590e028cdfcea2dad50c6d7a04957d
Davhot <david.home@mail.ru>
2019-01-14 22:18:03 +0300

    deploy

--- config/initializers/hot_catch_sender_logs.rb
+++ config/initializers/hot_catch_sender_logs.rb
@@ -1,3 +1,3 @@
 require 'sidekiq-cron'
 Rails.application.config.sender_logs = HotCatch::MakeHttpsRequest.new
-Sidekiq::Cron::Job.create(name: 'NginxSystemWorker - every 1min', cron: '*/1 * * * *', class: 'NginxSystemWorker')
+# Sidekiq::Cron::Job.create(name: 'NginxSystemWorker - every 1min', cron: '*/1 * * * *', class: 'NginxSystemWorker')
