4438b95de77163377df6f9988fc7c72000172dfa
Хотелов Давид Александрович <dkhotelov@phoenixit.ru>
2019-08-17 22:29:31 +0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '77.37.218.243', user: 'david', roles: %w{app db}, port: 8000
 
-set :branch, ENV['branch'] || 'develop'
+set :branch, ENV['branch'] || 'deploy'
 
 set :default_env, {
   'RAILS_ENV': 'production'
